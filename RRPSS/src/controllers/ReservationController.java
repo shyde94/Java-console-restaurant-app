f34a559faf4690f330926f31a2620fd4e5d0373f
Shide <shyde.foo@gmail.com>
2016-10-28 23:33:29 +0800

    update

--- RRPSS/src/controllers/ReservationController.java
+++ RRPSS/src/controllers/ReservationController.java
@@ -4,34 +4,12 @@ import entity.TableAll;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.Date;
+import java.util.InputMismatchException;
 
 import entity.Reservation;
 
 public class ReservationController{
-	/*when reservation controller instantiated, compare reservationAll with day, and with tableMaster? set those tables
-	 * which are reserved on that day to reserve. 
-	 * 
-	 * 
-	 * */
-	/*
-	 Code for UI to delete reservation.
-	  ArrayList<Reservation> tempList = new ArrayList<Reservation>();
-	for(int i=0;i<reservationList.size();i++){
-		Reservation temp = reservationList.get(i);
-		if(temp.getHpNumber() == hpNumber && temp.getCustomerName() == customerName){
-			tempList.add(temp); //In case customer makes multiple reservations.
-		}
-	}
-	if(tempList.isEmpty()){
-		System.out.println("There is no reservation under " + customerName + ". ");
-	}
-	else{
-		System.out.println("Multiple reservations under given name: ");
-		for(int i=0;i<tempList.size();i++){
-			System.out.print(i);
-			tempList.get(i).printReservation();
-		}
-		System.out.println*/
+	
 	private ReservationAll allTheReservations;
 	private TableAll allTheTables;
 	
@@ -41,5 +19,78 @@ public class ReservationController{
 		allTheTables = new TableAll();
 	}
 	
+	public void run() throws InputMismatchException{ 
+		boolean x = true; 
+		Scanner input = new Scanner(System.in);
+		while(x){
+			boolean y = true;
+			this.displayMenuOptions();
+			int choice = input.nextInt();
+			switch(choice){
+				case(1):
+					do{
+						try{
+							this.createReservation();
+							y = false;
+						}
+						catch(InputMismatchException e){
+							System.out.println("Invalid option");
+						}
+					}while(y);
+					break;
+				case(2): 
+					do{
+						try{
+							this.checkReservation();
+							y = false;
+						}
+						catch(InputMismatchException e){
+							System.out.println("Invalid option");
+						}
+					}while(y);
+					break;
+					
+				case(3): 
+					do{
+						try{
+							this.removeReservation();
+							y = false;
+						}
+						catch(InputMismatchException e){
+							System.out.println("Invalid Input. 3");
+						}
+						catch(ArrayIndexOutOfBoundsException e){
+							System.out.println("There is no such option!");
+						}
+					}while(y);
+					break;
+				case(-1):return;
+				default: System.out.println("Please select a valid option");
+			}
+		}
+	}
+	
+	private void removeReservation() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void checkReservation() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void createReservation() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void displayMenuOptions(){		//Display main menu options
+		System.out.println("Menu Options (Enter -1 to go back)");
+		System.out.println("1. Create new reservation");
+		System.out.println("2. Check reservation");
+		System.out.println("3. Remove reservation");
+		
+	}
 	
 }
